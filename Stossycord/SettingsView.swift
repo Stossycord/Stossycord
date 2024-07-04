dfff41926c04f7c9858f8f166d6228748f1d6916
TreeTops <158498287+TheTreeTops@users.noreply.github.com>
2024-07-03 20:33:55 -0400

    Better Settings

--- Stossycord/SettingsView.swift
+++ Stossycord/SettingsView.swift
@@ -5,6 +5,13 @@
 //  Created by Stossy11 on 30/5/2024.
 //
 
+//
+//  SettingsView.swift
+//  Stossycord
+//
+//  Created by Stossy11 on 30/5/2024.
+//
+
 import SwiftUI
 import KeychainSwift
 import Starscream
@@ -17,15 +24,39 @@ struct SettingsView: View {
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        List {
-            Toggle("Show All Server Emojis (most will not work unless you have nitro)", isOn: $webSocketClient.hasnitro)
-            Toggle("Enable TTS When Message Sent", isOn: $webSocketClient.hastts)
-            Button("Log Out") {
-                keychain.set("", forKey: "token")
-                token = ""
-                hasbeenopened = true
-                self.presentationMode.wrappedValue.dismiss()
+        NavigationView {
+            VStack {
+                Form {
+                    Section(header: Text("Settings")) {
+                        Toggle("Show All Server Emojis (most will not work unless you have nitro)", isOn: $webSocketClient.hasnitro)
+                            .toggleStyle(SwitchToggleStyle(tint: .blue))
+                        Toggle("Enable TTS When Message Sent", isOn: $webSocketClient.hastts)
+                            .toggleStyle(SwitchToggleStyle(tint: .blue))
+                    }
+
+                    Section {
+                        Button(action: {
+                            keychain.set("", forKey: "token")
+                            token = ""
+                            hasbeenopened = true
+                            self.presentationMode.wrappedValue.dismiss()
+                        }) {
+                            Text("Log Out")
+                                .frame(maxWidth: .infinity, alignment: .center)
+                                .padding()
+                                .foregroundColor(.white)
+                                .background(Color.red)
+                                .cornerRadius(10)
+                        }
+                    }
+                }
+                Text("App Version 0.0.6")
+                    .font(.headline)
+                    .foregroundColor(.gray)
+                    .padding()
             }
+            .padding()
+            .background(Color(.systemGroupedBackground).edgesIgnoringSafeArea(.all))
         }
     }
 }
