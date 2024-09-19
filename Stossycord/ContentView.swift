e49c0bbf24d1d43dcb959530ce68b33761df4ff6
stossy11 <69031796+stossy11@users.noreply.github.com>
2024-09-19 13:52:15 +1000

    Initial Commit

---
+++ Stossycord/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stossycord
+//
+//  Created by Stossy11 on 19/9/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stossycord/StossycordApp.swift
@@ -0,0 +1,17 @@
+//
+//  StossycordApp.swift
+//  Stossycord
+//
+//  Created by Stossy11 on 19/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct StossycordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
